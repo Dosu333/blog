5ddb500a4e269f5658db5df16c0b4b5ef8728263
Dosu333 <vladamir1865@gmail.com>
2020-11-26 15:12:16 +0100

    created views for creating and managing users

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+from rest_framework import generics, permissions
+
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated)
+
+    def get_object(self):
+        return self.request.user
