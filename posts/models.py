139ab442dfe1edf11560f44702092083c701d4df
Dosu333 <vladamir1865@gmail.com>
2020-11-27 10:21:42 +0100

    created comments model

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 import uuid
 # Create your models here.
@@ -16,7 +19,13 @@ class Post(BaseModel):
     category = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=255)
     body = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return self.title
+
+class Comment(BaseModel):
+    author = models.CharField(max_length=255)
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
